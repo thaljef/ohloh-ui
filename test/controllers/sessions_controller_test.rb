cf85719afbee18f4f9ba295172a9b9b5b636bc56
Alex Johnson <notalexjohnson@gmail.com>
2019-08-02 17:04:36 +0530

    OTWO-5690 Add health route for monitoring

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -117,4 +117,18 @@ describe 'SessionsController' do
       end
     end
   end
+
+  describe 'health' do
+    it 'must return the current timestamp when DB is accessible' do
+      get :health
+      must_respond_with :success
+      response.body.must_match Time.current.strftime('%F %H')
+    end
+
+    it 'must return status 500 when DB is inaccessible' do
+      ActiveRecord::Base.establish_connection(adapter: :postgresql, database: 'foobar')
+      get :health
+      must_respond_with :forbidden
+    end
+  end
 end
